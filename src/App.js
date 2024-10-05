27c993b8e26d980aebe12bf04ad10ab576897367
johanfortus <fortusjohan@gmail.com>
2024-10-05 19:11:30 -0400

    Set up React with Monaco editor, modified package.json after installing react-scripts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Editor from '@monaco-editor/react';
 
 function App() {
+    const [code, setCode] = useState('// Write your Verse code here');
+
+    const handleEditorChange = (value) => {
+        setCode(value);
+    };
+
     return (
         <div>
             <h1>Verse Playground</h1>
+            <Editor
+                height="90vh"
+                defaultLanguage="javascript"
+                value={code}
+                onChange={handleEditorChange}
+                theme="vs-dark"
+            />
         </div>
     );
 }
