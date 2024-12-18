545331c719fe539c4cc241c95231cf16c3d914a6
johanfortus <fortusjohan@gmail.com>
2024-12-17 20:54:04 -0500

    fixed server issues

--- src/verse-lsp-client.js
+++ src/verse-lsp-client.js
@@ -1,32 +1,31 @@
 import { MonacoLanguageClient } from 'monaco-languageclient';
-import { WebSocketMessageReader, WebSocketMessageWriter } from 'vscode-ws-jsonrpc';
+import { WebSocketMessageReader, WebSocketMessageWriter, toSocket } from 'vscode-ws-jsonrpc';
 import { createMessageConnection } from 'vscode-jsonrpc/browser.js';
-// import { createMessageConnection } from 'vscode-jsonrpc';
-// import { connect } from 'http2';
 
 const url = 'ws://localhost:3001';
 const webSocket = new WebSocket(url);
 
 webSocket.onopen = () => {
-  const reader = new WebSocketMessageReader(webSocket);
-  const writer = new WebSocketMessageWriter(webSocket);
+    const socket = toSocket(webSocket);
+    const reader = new WebSocketMessageReader(socket);
+    const writer = new WebSocketMessageWriter(socket);
 
-  const connection = createConnection(reader, writer);
-  connection.listen();
+    const connection = createMessageConnection(reader, writer);
+    connection.listen();
 
-  const languageClient = new MonacoLanguageClient({
-    name: 'Verse Language Client',
-    clientOptions: {
-      documentSelector: ['verse'],
-      synchronize: {
-        configurationSection: 'verse',
-        fileEvents: []
-      }
-    },
-    connectionProvider: {
-      get: () => Promise.resolve(connection)
-    }
-  });
+    const languageClient = new MonacoLanguageClient({
+        name: 'Verse Language Client',
+        clientOptions: {
+            documentSelector: ['verse'],
+            synchronize: {
+                configurationSection: 'verse',
+                fileEvents: []
+            }
+        },
+        connectionProvider: {
+        get: () => Promise.resolve(connection)
+        }
+    });
 
-  languageClient.start();
+    languageClient.start();
 };
